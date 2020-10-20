13e517a119f3cb372043c209247747ab00817cce
Berry <40413510+b1u3b3rr7@users.noreply.github.com>
2020-10-20 11:27:24 +0900

    update

--- api/objects/product.php
+++ api/objects/product.php
@@ -127,4 +127,81 @@ class Product
 
         return false;
     }
+
+    // delete the product
+    function delete()
+    {
+        // delete query
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind id of record to delete
+        $stmt->bindParam(1, $this->id);
+
+        // execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // search products
+    function search($keywords)
+    {
+        // select all query
+        $query = "SELECT c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created FROM " . $this->table_name . " p LEFT JOIN categories c ON p.category_id = c.id WHERE p.name LIKE ? OR p.description LIKE ? OR c.name LIKE ? ORDER BY p.created DESC";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $keywords = htmlspecialchars(strip_tags($keywords));
+        $keywords = "%{$keywords}%";
+
+        // bind
+        $stmt->bindParam(1, $keywords);
+        $stmt->bindParam(2, $keywords);
+        $stmt->bindparam(3, $keywords);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // read products with pagination
+    public function readPaging($from_record_num, $records_per_page) {
+        // select query
+        $query = "SELECT c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created FROM " . $this->table_name . " p LEFT JOIN categories c ON p.category_id = c.id ORDER BY p.created DESC LIMIT ?, ?";
+        
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // bind variable values
+        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+
+        // execute query
+        $stmt->execute();
+
+        // return values from database
+        return $stmt;
+    }
+
+    // used for paging products
+    public function count() {
+        $query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name . "";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $row['total_rows'];
+    }
 }
